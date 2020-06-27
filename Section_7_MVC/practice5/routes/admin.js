7cc7d04d1c87acdf545c91b8fbf5c1a814a6f0b2
sliverz6 <52848569+sliverz6@users.noreply.github.com>
2020-06-27 21:35:47 +0900

    2020/06/27

---
+++ Section_7_MVC/practice5/routes/admin.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const productController = require('../controller/products');
+
+const router = express.Router();
+
+router.get('/add-product', productController.getAddProduct);
+
+router.post('/add-product', productController.postAddProduct)
+
+module.exports = router;
